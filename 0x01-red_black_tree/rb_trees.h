b459acb03ec988ea9f815b832061e9389f6de447
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2023-03-22 23:02:10 -0500

    add prototype

--- 0x01-red_black_tree/rb_trees.h
+++ 0x01-red_black_tree/rb_trees.h
@@ -40,6 +40,9 @@ typedef struct rb_tree_s
     struct rb_tree_s *right;
 } rb_tree_t;
 
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
+
+
 /* 1-rb_tree_is_valid.c */
 int rb_tree_is_valid(const rb_tree_t *tree);
 int is_bst(const rb_tree_t *tree, int min, int max);
