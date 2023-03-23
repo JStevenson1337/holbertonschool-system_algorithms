d2686a2cb2f3a57371463111fee95127b2e50f67
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2023-03-23 18:40:36 -0500

    corrected for betty

--- 0x01-red_black_tree/3-array_to_rb_tree.c
+++ 0x01-red_black_tree/3-array_to_rb_tree.c
@@ -1,12 +1,10 @@
 #include "rb_trees.h"
-
-
 /**
  * array_to_rb_tree - builds a Red-Black Tree from an array
  *
- * @array: array of integers, may be unsorted and have repeating values
- * @size: amount of array members
- * Return: pointer to head of new RBT constructed from array
+ * @array: pointer to the first element of the array to be converted
+ * @size: the number of elements in the array
+ * Return: pointer to the root node of the created R-B tree, or NULL on failure
  */
 rb_tree_t *array_to_rb_tree(int *array, size_t size)
 {
@@ -18,18 +16,13 @@ rb_tree_t *array_to_rb_tree(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		/* check if NULL return is for repeat value */
 		if (rb_tree_insert(&tree, array[i]) == NULL)
 		{
 			for (j = 0; j < size && array[j] != array[i]; j++)
 			{}
-
-			/* not a repeating value, bst_insert failure */
 			if (j == i)
 				return (NULL);
 		}
 	}
-
 	return (tree);
-
 }
