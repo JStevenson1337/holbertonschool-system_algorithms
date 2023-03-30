cc267541732fa86de3598edcd3f39058e5312e36
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2023-03-29 19:49:49 -0500

    checking task 0

--- 0x02-graphs/0-graph_create.c
+++ 0x02-graphs/0-graph_create.c
@@ -1,13 +1,18 @@
 #include "graphs.h"
-
-/*
+/**
+ * graph_create - initialize a graph structure
  *
- *
- *
- *
-*/
+ * Return: pointer to graph
+ **/
 graph_t *graph_create(void)
-  {
+{
+	graph_t *graph = (graph_t *)malloc(sizeof(graph_t));
 
+	if (graph)
+	{
+		graph->nb_vertices = 0;
+		graph->vertices = NULL;
+	}
 
-  }
+	return (graph);
+}
