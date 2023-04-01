b63847c68e2895718de3ac0ff2a61ac2c34eac3c
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2023-03-31 22:23:33 -0500

    check 9

--- 0x00-huffman_coding/heap/heap_create.c
+++ 0x00-huffman_coding/heap/heap_create.c
@@ -1,9 +1,28 @@
 #include "heap.h"
-
+/**
+ * heap_create - determine if a node’s value is greater or lower than another
+ * 
+ * @data_cmp: pointer to a comparison function
+ * Return - pointer to created heap or NULL
+ */
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
 
-	
+	(void)data_cmp;
+
+	heap_t *heap = (heap_t *)malloc(sizeof(heap_t));
+
+	if (heap == NULL)
+	{
+		return (NULL);
+	}
+
+	heap->root = NULL;
+	heap->size = 0;
+
+	return (heap);
+
+
 }
 
 
