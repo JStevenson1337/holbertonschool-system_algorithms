f5c7c25c7f9ad6bd451ce093ce198cd051ca2500
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2023-04-09 05:24:12 -0500

    Add Pathfinding Lesson

---
+++ 0x01-pathfinding/0-backtracking_array.c
@@ -0,0 +1,10 @@
+#include "pathfinding.h"
+/*
+ *
+ *
+ *
+*/
+queue_t *backtracking_array(char **map, int rows, int cols, point_t const *start, point_t const *target)
+{
+
+}
---
+++ 0x01-pathfinding/pathfinding.h
@@ -0,0 +1,19 @@
+#ifndef __PATHFINDING_H__
+#define __PATHFINDING_H__
+
+
+#include <stdio.h>
+
+/**
+ * struct point_s - Structure storing coordinates
+ *
+ * @x: X coordinate
+ * @y: Y coordinate
+ */
+typedef struct point_s
+{
+    int x;
+    int y;
+} point_t;
+
+#endif /* __PATHFINDING_H__ */
