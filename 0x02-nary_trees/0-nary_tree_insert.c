07e1ae2b6d3d890bef0e4265da2650f52e8e76e8
jstevenson1337 <jmstevenson918@gmail.com>
2023-04-26 12:59:13 -0500

    remove mangled name

--- 0x02-nary_trees/0-nary_tree_insert.c
+++ 0x02-nary_trees/0-nary_tree_insert.c
@@ -7,7 +7,7 @@
  *
  * Return - pointer to the new node or NULL if fail
  */
-nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *_str)
+nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 {
 	nary_tree_t *new = calloc(1, sizeof(*new));
 	char *str = strdup(_str);
